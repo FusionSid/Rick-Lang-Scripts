1f2791b582a5278f3507053f8e67efdc54ef18c6
FusionSid <siddheshadsv@icloud.com>
2022-03-28 19:05:58 +1300

    explain the small script

--- rick-lang.sh
+++ rick-lang.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# i added this as an alias called ricklang in my .zshrc so i can do `ricklang <file>`` to run a rick roll file
+# This is a simple script I put so i dont have to have the repo in my folder every time i wanna run a rickroll script
+# I have an alias called "ricklang" in my .zshrc file so when ever i do `ricklang <file>` it will run the rick roll file
+
+# Saves the current directory path
 cwd=$(pwd)
+
+# Goes to the folder with the rick lang file 
 cd /Users/siddheshzantye/rick-lang/;
+
+# I renamed the RickRoll.py file to run and made it an executable
 ./run "$cwd/$1"
