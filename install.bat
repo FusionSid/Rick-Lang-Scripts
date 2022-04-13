51d5ea530226d1838a55e1b0b0f27006c33f2a71
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-04-13 14:49:08 +0400

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,8 @@
+echo Welcome Lets get started
+cd /d "%HOMEDRIVE%%HOMEPATH%"
+curl https://github.com/Rick-Lang/rickroll-lang/archive/refs/heads/main.zip -O -J -L
+tar -xf rickroll-lang-main.zip
+DEL rickroll-lang-main.zip
+echo "Finished downloading scripts\nNow installing ricklang command..."
+
+cd /d "%HOMEDRIVE%%HOMEPATH%"/rickroll-lang-main/src-py/
